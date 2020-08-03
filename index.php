9fa6a1c3947f8804064b6a50bfcf4c97c7c9caf7
godJah <god@fastfen.club>
2020-08-03 19:49:55 +0300

    php cs fixer added

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
     declare(strict_types=1);
-    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/btcrelax/bitganj-core/src/core.php';
     $core = \BtcRelax\Core::getIstance();
     $core->run();
