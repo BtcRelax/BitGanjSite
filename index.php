c16c0efb50af8a395ecb303db0f04fe4399e3f25
CodeMasterBoom <boomhacker@cryptopup.space>
2020-08-04 22:11:21 +0300

    autoload via composer

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-    $core_path = __DIR__ . '/vendor/btcrelax/bitganj-core/src/core.php';
+    $core_path = __DIR__ . '/vendor/autoload.php';
     if (\file_exists($core_path)) {
-    $core = \BtcRelax\Core::getIstance();
-    $core->run();
+        require_once $core_path;
+        $core = \BtcRelax\Core::getIstance();
+        $core->run();
     } else {
         echo "You need to run: composer install ";
     } 
