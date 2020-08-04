3759e424c5d6d43f5f8dd760a83cec49fa678221
godJah <god@fastfen.club>
2020-08-04 04:14:28 +0300

    xdebug for vs

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-    require __DIR__ . '/vendor/autoload.php';
+    require __DIR__ . '/vendor/btcrelax/bitganj-core/src/core.php';
     $core = \BtcRelax\Core::getIstance();
     $core->run();
