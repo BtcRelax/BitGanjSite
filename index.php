9a8a7bba010115cc5563ae96f9148150a746db08
godJah <god@fastfen.club>
2020-08-04 18:05:44 +0300

    check are dependencies installed

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
-    require __DIR__ . '/vendor/btcrelax/bitganj-core/src/core.php';
+    $core_path = __DIR__ . '/vendor/btcrelax/bitganj-core/src/core.php';
+    if (\file_exists($core_path)) {
     $core = \BtcRelax\Core::getIstance();
     $core->run();
+    } else {
+        echo "You need to run: composer install ";
+    } 
+    
