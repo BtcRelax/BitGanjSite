6aabc0d8d8f093b72818263ca8a91014306a7995
godJah <god@fastfen.club>
2020-08-13 11:24:19 +0300

    PSR-2

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
-    $core_path = __DIR__ . '/vendor/autoload.php';
-    if (\file_exists($core_path)) {
-        include_once $core_path;
-        $core = \BtcRelax\Core::getIstance();
-        $core->run();
-    } else {
-        echo "You need to run: composer install ";
-    }
+
+/**
+ * @file
+ * Description: Main entry point
+ */
+
+$core_path = __DIR__ . '/vendor/autoload.php';
+if (\file_exists($core_path)) {
+    include_once $core_path;
+    $core = \BtcRelax\Core::getIstance();
+    $core->run();
+} else {
+    echo "You need to run: composer install ";
+}
