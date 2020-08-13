64ac320922b23f6b3cf6c7f8de6669e2302c09d5
godJah <god@fastfen.club>
2020-08-13 08:39:45 +0300

    Added phpunit 9 via composer

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
     $core_path = __DIR__ . '/vendor/autoload.php';
     if (\file_exists($core_path)) {
-        require_once $core_path;
+        include_once $core_path;
         $core = \BtcRelax\Core::getIstance();
         $core->run();
     } else {
         echo "You need to run: composer install ";
-    } 
-    
+    }
