82688573d3e4a9649245116fc5be8203dc2b44d6
godJah <god@fastfen.club>
2020-08-13 11:31:19 +0300

    add doc section for PSR 2

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 
 /**
- * @file
  * Description: Main entry point
+ * php version 7.4
+ *
+ * @category   Application
+ * @package    BtcRelax
+ * @subpackage Site
+ * @author     god Jah <god@fastfen.club>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://yoursite.com
+ * @since      1.0.0
  */
 
 $core_path = __DIR__ . '/vendor/autoload.php';
