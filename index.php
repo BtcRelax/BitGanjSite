363bbc4cc1fda6fb7b558a788653b4e69386389b
godJah <god@fastfen.club>
2020-08-20 06:33:09 +0300

    correct core call

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 $core_path = __DIR__ . '/vendor/autoload.php';
 if (\file_exists($core_path)) {
     include_once $core_path;
-    $core = \BtcRelax\Core::getIstance();
+    $core = \BtcRelax\Core::getInstance();
     $core->run();
 } else {
     echo "You need to run: composer install ";
